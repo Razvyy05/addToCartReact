2391655f7b75aaf0423bea6927b27db7845d44a6
Razvan Iovan <razvaniovan05@yahoo.com>
2023-08-29 13:50:28 +0200

    New adds

---
+++ src/products.js
@@ -0,0 +1,45 @@
+import product1 from './assets/Img1.jpg';
+import product2 from './assets/Img2.jpg';
+import product3 from './assets/Img3.jpg';
+import product4 from './assets/Img4.jpg';
+import product5 from './assets/Img5.jpg';
+import product6 from './assets/Img6.jpg';
+
+export const PRODUCTS = [
+    {
+        id: 1,
+        productName: "IPhone",
+        price: 999.0,
+        productImage: product1,
+    },
+    {
+        id: 2,
+        productName: "MacBook Pro",
+        price: 1999.0,
+        productImage: product2,
+    },
+    {
+        id: 3,
+        productName: "Samsung Galaxy",
+        price: 899.0,
+        productImage: product3,
+    },
+    {
+        id: 4,
+        productName: "TV Samsung",
+        price: 1299.0,
+        productImage: product4,
+    },
+    {
+        id: 5,
+        productName: "Samsung Tablet",
+        price: 299.0,
+        productImage: product5,
+    },
+    {
+        id: 6,
+        productName: "IWatch",
+        price: 1299.0,
+        productImage: product6,
+    }
+]
